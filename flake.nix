c06f164fa78428146bd31814286f8883e5ff5518
Alan <o@alanj.in>
2025-06-25 11:26:07 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    # NixOS official package source, using the nixos-25.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix     
+      	home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.alan = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "alan";
+  home.homeDirectory = "/home/alan";
+
+  home.packages = with pkgs; [
+    nnn # terminal file manager
+
+    zip
+    xz
+    unzip
+    p7zip
+
+    eza # A modern replacement for ‘ls’
+
+    which
+    nix-output-monitor
+    lsof
+    ethtool
+    pciutils
+    usbutils
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Alan";
+    userEmail = "dev@alanj.in";
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin"
+    '';
+
+    shellAliases = {
+      vim = "nvim";
+    };
+  };
+
+  home.stateVersion = "25.05";
+}
